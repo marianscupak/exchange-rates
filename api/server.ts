f6c5d4d8fa3968fccb14c5a88033fac8863bcfcb
František Pác <frantisek.pac@iot-water.cz>
2024-09-25 14:21:05 +0200

    add `serve.ts` utilizing express

---
+++ api/server.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+
+import express, { Request, Response } from "express";
+
+import { fetchExchangeRates } from "./_api.helpers";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const handler = async (req: Request, res: Response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+
+    const result = await fetchExchangeRates();
+    res.status(200).send(result);
+};
+
+app.get("/api/exchange-rates", handler);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
